c833ba85164d806d099f73474f5dcd5fe6702626
Copy Liu <copyliu@gmail.com>
2014-09-01 13:58:04 +0800

    add some ani

--- Bililive_dm/DanmakuTextControl.xaml.cs
+++ Bililive_dm/DanmakuTextControl.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -21,6 +22,13 @@ namespace Bililive_dm
 		public DanmakuTextControl()
 		{
 			this.InitializeComponent();
+            this.Loaded += DanmakuTextControl_Loaded;
 		}
+
+        void DanmakuTextControl_Loaded(object sender, RoutedEventArgs e)
+        {
+//            this.Loaded-=DanmakuTextControl_Loaded;
+//            this.BeginStoryboard((Storyboard)this.Resources["Storyboard1"]);
+        }
 	}
 }
