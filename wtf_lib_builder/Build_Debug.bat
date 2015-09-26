326fb1dfa72ecab64d0ed4965de3fafebaa897a8
Copy Liu <copyliu@gmail.com>
2015-09-27 01:29:38 +0800

    修復發佈build

--- wtf_lib_builder/Build_Debug.bat
+++ wtf_lib_builder/Build_Debug.bat
@@ -1,2 +1,4 @@
 MSBuild /nologo /verbosity:m /maxcpucount /p:Configuration="Debug",Platform="Win32",SolutionDir="../" "../libwtfdanmaku/libwtfdanmaku.vcxproj"
 MSBuild /nologo /verbosity:m /maxcpucount /p:Configuration="Debug",Platform="x64",SolutionDir="../" "../libwtfdanmaku/libwtfdanmaku.vcxproj"
+copy ..\Debug\Win32\*.dll ..\Bililive_dm\Win32\
+copy ..\Debug\x64\*.dll ..\Bililive_dm\x64\
--- wtf_lib_builder/Build_Release.bat
+++ wtf_lib_builder/Build_Release.bat
@@ -1,2 +1,4 @@
 MSBuild /nologo /verbosity:m /maxcpucount /p:Configuration="Release",Platform="Win32",SolutionDir="../" "../libwtfdanmaku/libwtfdanmaku.vcxproj"
 MSBuild /nologo /verbosity:m /maxcpucount /p:Configuration="Release",Platform="x64",SolutionDir="../" "../libwtfdanmaku/libwtfdanmaku.vcxproj"
+copy ..\Release\Win32\*.dll ..\Bililive_dm\Win32\
+copy ..\Release\x64\*.dll ..\Bililive_dm\x64\
