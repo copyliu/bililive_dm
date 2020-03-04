839156881b84f0742e2c0b82229ba67c22584eb4
SilverN <hmxy101@yahoo.com>
2020-03-04 15:37:14 +0800

    保留必要模块

--- wtf_lib_builder/Build_Release.bat
+++
@@ -1,4 +0,0 @@
-MSBuild /nologo /verbosity:m /maxcpucount /p:Configuration="Release",Platform="Win32" "../libwtfdanmaku/libwtfdanmaku.vcxproj"
-MSBuild /nologo /verbosity:m /maxcpucount /p:Configuration="Release",Platform="x64" "../libwtfdanmaku/libwtfdanmaku.vcxproj"
-copy ..\libwtfdanmaku\Release\Win32\*.dll ..\Bililive_dm\Win32\
-copy ..\libwtfdanmaku\Release\x64\*.dll ..\Bililive_dm\x64\
