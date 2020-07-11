91b1d0c91b3ca0856ef7a2b90e4cec93705df140
Copy Liu <copyliu@gmail.com>
2020-07-12 00:40:13 +0800

    bump version

--- Bililive_dm/MainWindow.xaml.cs
+++ Bililive_dm/MainWindow.xaml.cs
@@ -94,7 +94,10 @@ namespace Bililive_dm
             }
             HelpWeb.Navigated+=HelpWebOnNavigated;
             //初始化日志
-
+            if (!(Debugger.IsAttached ))
+            {
+                this.IsEnabled = false;
+            }
 
             try
             {
