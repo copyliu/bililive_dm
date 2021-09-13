25e88efe801d6e2fc92b1bc3b0b82efc5bd16b6b
Copy Liu <copyliu@gmail.com>
2021-09-13 21:53:56 +0800

    Squashed commit of the following:
    commit f1d66c719a372fb76074d5e63759b908a436842d
    Merge: ef2e1d6 eeda8ce
    Author: Copy Liu <copyliu@gmail.com>
    Date:   Mon Sep 13 21:49:47 2021 +0800
    
        Merge branch 'master' of https://github.com/iRedmond/bililive_dm into iRedmond-master
    
    commit eeda8ce7e70d740f735b4e57a4f59a4f668ef477
    Author: iRedmond <i@redmond.ms>
    Date:   Wed Sep 8 16:39:11 2021 +0800
    
        Simple Styles.xaml is back, and it actually works now!
    
    commit ddf1ae6f64c72a2dae5596e91b32d4d73f237769
    Author: iRedmond <i@redmond.ms>
    Date:   Mon Sep 6 15:45:13 2021 +0800
    
        fix dirs

--- wtf_lib_builder/Build_Debug.bat
+++ wtf_lib_builder/Build_Debug.bat
@@ -1,4 +1,7 @@
 MSBuild /nologo /verbosity:m /maxcpucount /p:Configuration="Debug",Platform="Win32" "../libwtfdanmaku/libwtfdanmaku.vcxproj"
 MSBuild /nologo /verbosity:m /maxcpucount /p:Configuration="Debug",Platform="x64" "../libwtfdanmaku/libwtfdanmaku.vcxproj"
-copy ..\libwtfdanmaku\Debug\Win32\*.dll ..\Bililive_dm\Win32\
-copy ..\libwtfdanmaku\Debug\x64\*.dll ..\Bililive_dm\x64\
+pushd ..
+md Bililive_dm\Win32
+copy libwtfdanmaku\Debug\Win32\*.dll Bililive_dm\Win32\
+md Bililive_dm\x64
+copy libwtfdanmaku\Debug\x64\*.dll Bililive_dm\x64\
