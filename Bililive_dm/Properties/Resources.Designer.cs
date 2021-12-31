d26b37ed608761dedaa9d20d0e234e307fcfa5ed
Copy Liu <copyliu@gmail.com>
2022-01-01 01:13:00 +0800

    update year

--- Bililive_dm/Properties/Resources.Designer.cs
+++ Bililive_dm/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Bililive_dm.Properties {
     // 类通过类似于 ResGen 或 Visual Studio 的工具自动生成的。
     // 若要添加或移除成员，请编辑 .ResX 文件，然后重新运行 ResGen
     // (以 /str 作为命令选项)，或重新生成 VS 项目。
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -61,7 +61,7 @@ namespace Bililive_dm.Properties {
         }
         
         /// <summary>
-        ///   查找类似 非B站出品 ©2014-2021 CopyLiu 放棄所有權利 房間編號5051 問題反饋 copyliu@gmail.com 的本地化字符串。
+        ///   查找类似 非B站出品 ©2014-2022 CopyLiu 放棄所有權利 房間編號5051 問題反饋 copyliu@gmail.com 的本地化字符串。
         /// </summary>
         public static string ABout_Text {
             get {
@@ -358,7 +358,7 @@ namespace Bililive_dm.Properties {
         }
         
         /// <summary>
-        ///   查找类似 发生了未知的互动事件 的本地化字符串。
+        ///   查找类似 {0} 发生了未知的互动事件 的本地化字符串。
         /// </summary>
         public static string InteractType_Unknown {
             get {
@@ -899,6 +899,24 @@ namespace Bililive_dm.Properties {
         }
         
         /// <summary>
+        ///   查找类似 未啟用 的本地化字符串。
+        /// </summary>
+        public static string Plugin_DataGrid_Status_Off {
+            get {
+                return ResourceManager.GetString("Plugin_DataGrid_Status_Off", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   查找类似 已啟用 的本地化字符串。
+        /// </summary>
+        public static string Plugin_DataGrid_Status_On {
+            get {
+                return ResourceManager.GetString("Plugin_DataGrid_Status_On", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   查找类似 投喂榜 的本地化字符串。
         /// </summary>
         public static string RankTabItem_Header {
@@ -1192,24 +1210,6 @@ namespace Bililive_dm.Properties {
         public static string WTFEngineEnable_Content {
             get {
                 return ResourceManager.GetString("WTFEngineEnable.Content", resourceCulture);
-            }
-        }
-
-        /// <summary>
-        ///   查找类似 已啟用 的本地化字符串。
-        /// </summary>
-        public static string Plugin_DataGrid_Status_On {
-            get {
-                return ResourceManager.GetString("Plugin_DataGrid_Status_On", resourceCulture);
-            }
-        }
-
-        /// <summary>
-        ///   查找类似 未啟用 的本地化字符串。
-        /// </summary>
-        public static string Plugin_DataGrid_Status_Off {
-            get {
-                return ResourceManager.GetString("Plugin_DataGrid_Status_Off", resourceCulture);
             }
         }
     }
