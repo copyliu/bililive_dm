2a333f5b79cdd175b4009d8c57ed27a70e0add11
Copy Liu <copyliu@gmail.com>
2022-05-24 23:43:26 +0800

    开始准备MAUI

---
+++ BililliveCmtViewerCore/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BililliveCmtViewerCore
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
