f697e1f4b5b1fe7666e256dfe2b1ed57ccdd3ead
Copy Liu <copyliu@gmail.com>
2023-10-01 17:57:47 +0800

    加一個單元測試. 測一下上艦的事情

---
+++ BiliDMLib.Tests/Tests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using BilibiliDM_PluginFramework;
+using Xunit;
+
+namespace BiliDMLib.Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void GuardTest()
+        {
+            var guardjson =
+                "{\n  \"cmd\": \"LIVE_OPEN_PLATFORM_GUARD\",\n  \"data\": {\n    \"user_info\": {\n      \"uid\": 110000331,\n      \"uname\": \"\",\n      \"uface\": \"http://i0.hdslb.com/bfs/face/4add3acfc930fcd07d06ea5e10a3a377314141c2.jpg\"\n    },\n    \"guard_level\": 3,\n    \"guard_num\": 1,\n    \"guard_unit\": \"月\",\n    \"fans_medal_level\": 24,\n    \"fans_medal_name\": \"aw4ifC\",\n    \"fans_medal_wearing_status\": false,\n    \"timestamp\": 1653555128,\n    \"room_id\": 460695,\n    \"msg_id\": \"\"\n  }\n}";
+            var model = new DanmakuModel(guardjson,2);
+            Assert.Equal(MsgTypeEnum.GuardBuy, model.MsgType);
+        }
+    }
+}
