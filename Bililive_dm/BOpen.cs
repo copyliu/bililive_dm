dd92a26e29ca300d83a350e9e9aa4a75b9a939c6
Copy Liu <copyliu@gmail.com>
2026-04-09 21:19:28 +0800

    更換伺服器

--- Bililive_dm/BOpen.cs
+++ Bililive_dm/BOpen.cs
@@ -73,13 +73,26 @@ namespace Bililive_dm
 
                 var param = JsonConvert.SerializeObject(new { code = code, app_id = 1651388990835 }, Formatting.None);
                 HttpResponseMessage req;
+               
                 try
                 {
-                    req = await httpClient.PostAsync("https://bopen.ceve-market.org/sign", new StringContent(param, Encoding.UTF8, "application/json"));
+                    req = await httpClient.PostAsync("https://soft.ceve-market.org/bopen/sign", new StringContent(param, Encoding.UTF8, "application/json"));
+                    if (!req.IsSuccessStatusCode)
+                    {
+                        throw new NotSupportedException(Resources.BOpen_GetRoomIdByCode_簽名伺服器離線);
+                    }
                 }
                 catch (Exception e)
                 {
-                    throw new NotSupportedException(Resources.BOpen_GetRoomIdByCode_簽名伺服器離線,e);
+                    try
+                    {
+                        req = await httpClient.PostAsync("https://bopen.ceve-market.org/sign", new StringContent(param, Encoding.UTF8, "application/json"));
+                    }
+                    catch (Exception e1)
+                    {
+                        throw new NotSupportedException(Resources.BOpen_GetRoomIdByCode_簽名伺服器離線,e1);
+                    }
+
                 }
                 if (!req.IsSuccessStatusCode)
                 {
